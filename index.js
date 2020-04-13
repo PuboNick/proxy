e53cd075b91b9b5839978155bd26bbb0875742e6
unknown <C3005882@sccd.SHZBG.COM>
2020-04-13 15:54:22 +0800

    fix(origin):修复低版本浏览器X-CSRF-TOKEN请求头导致跨域失败问题

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const now = () => moment().format("YYYY-MM-DD HH:mm:ss");
  */
 const middleWare = (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-type, Authorization, *');
+	res.header('Access-Control-Allow-Headers', 'Content-type, Authorization, X-CSRF-TOKEN, *');
 	res.header('Access-Control-Allow-Methods', '*');
 	next();
 };
